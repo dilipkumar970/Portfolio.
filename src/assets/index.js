6a9d51504b23a2654d238332304e1bb33d4ea016
Bonagiri Dilipkumar <bonagiridilipkumar970@gmail.com>
2025-07-04 21:27:12 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import gearXpert from './gearXpert.png';
+import project3 from './project3.webp';
+import project2 from './proj1.webp'
+import mysql from './tech/mysql.png';
+import aws from './tech/aws.png';
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import linux from "./tech/linux.png";
+import wordpress from "./tech/wordpress.png";
+import spring from "./tech/spring.png";
+import java from "./tech/java.png";
+import firstTestimonial from './Tars Ken.webp';
+import secondTestimonial from './image.webp';
+import thirdTestimonial from './third testimonial.webp'
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  aws,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+  figma,
+  git,
+  html,
+  javascript,
+  java,
+  linux,
+  wordpress,
+  spring,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+};
